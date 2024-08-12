fc9aa34f1277c1923c08c2e24993887c4940ad58
ProfessorLagg <futurecake2999@gmail.com>
2024-08-12 11:03:59 +0200

    auto-pre-20240812T110359

--- gitpush.cmd
+++ gitpush.cmd
@@ -9,7 +9,8 @@ SET pihostname=192.168.99.36
 
 echo %timestamp% > ".\version.txt"
 
-git add -A && git commit -m "auto-pre-%timestamp%" && git push
-scp -r pi@%pihostname%:/home/pi/IoT3/ .\ :: && xcopy .\IoT3 .\ /h /e /y /f && rmdir .\IoT3 /s /q
+git add -A && git commit -m "auto-pre-%timestamp%"
+scp -r pi@%pihostname%:/home/pi/IoT3/ .\
+:: && xcopy .\IoT3 .\ /h /e /y /f && rmdir .\IoT3 /s /q
 git add -A && git commit -m "auto-post-%timestamp%" && git push
 pause
