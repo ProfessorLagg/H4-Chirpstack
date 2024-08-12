548a30de4e2cf1eff36581cf8d227ff16b7497b9
ProfessorLagg <futurecake2999@gmail.com>
2024-08-12 11:52:42 +0200

    auto-20240812T115242

---
+++ gitpush.ps1
@@ -0,0 +1,38 @@
+Param(
+    [string]$piusername = 'pi',
+    [string]$pihostname = '192.168.99.36'
+)
+cd $PSScriptRoot
+
+$timestamp = [DateTime]::Now.ToString('yyyyMMdd\THHmmss')
+Set-Content -Value $timestamp -Path $(Join-Path -Path $PSScriptRoot -ChildPath "version.txt") -Encoding UTF8 -NoNewline -Force
+
+$ssh_user = $piusername + '@' + $pihostname
+
+
+&scp -r $ssh_user/home/pi/IoT3/ .\
+Get-ChildItem -Path ".\IoT3" | %{
+    Move-Item -Path $_.FullName -Destination ".\$($_.Name)" -Force
+}
+
+Remove-Item -Path ".\IoT3" -Force -Recurse
+&git add -A
+&git commit -m "auto-$($timestamp)"
+&git push
+
+#@ECHO OFF
+#cd %~dp0
+#
+#FOR /F "tokens=* USEBACKQ" %%F IN (`powershell -Command "return [DateTime]::Now.ToString('yyyyMMdd\THHmmss')"`) DO (
+#SET timestamp=%%F
+#)
+#
+#SET piusername=pi
+#SET pihostname=192.168.99.36
+#
+#echo %timestamp% > ".\version.txt"
+#
+#scp -r %piusername%@%pihostname%:/home/pi/IoT3/ .\
+#:: move /y IoT3 .
+#:: git add -A && git commit -m "auto-%timestamp%" && git push
+#pause
---
+++ readme.md
@@ -0,0 +1,9 @@
+## Description
+Make file to setup running Chirpstack from Docker on a Rasberry Pi 4
+## How to use
+``` sh
+sudo apt-get update && sudo apt update -y
+git clone "https://github.com/ProfessorLagg/H4-Chirpstack"
+cd H4-Chirpstack/AutoSetup
+make install-chirpstack
+```
