ccb9f557f8939a3ee9ec521fd08becb63ccd5adf
ProfessorLagg <futurecake2999@gmail.com>
2024-08-12 11:29:42 +0200

    removed gitpush.cmd from repo

--- gitpush.cmd
+++ gitpush.cmd
@@ -9,7 +9,6 @@ SET pihostname=192.168.99.36
 
 echo %timestamp% > ".\version.txt"
 
-::git add -A && git commit -m "auto-pre-%timestamp%"
 scp -r pi@%pihostname%:/home/pi/IoT3/ .\
 move /y IoT3 AutoSetup
 git add -A && git commit -m "auto-%timestamp%" && git push
