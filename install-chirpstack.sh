4566123b5159274fac0330b12906d9b4b28ef7cf
ProfessorLagg <futurecake2999@gmail.com>
2024-08-12 11:02:24 +0200

    auto-pre-20240812T110224

--- install-chirpstack.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-oldpath=$(realpath .)
-cd ~
-rm -rf chirpstack-docker
-git clone https://github.com/brocaar/chirpstack-docker.git
-cd chirpstack-docker
-sudo docker-compose up
-cd $oldpath
